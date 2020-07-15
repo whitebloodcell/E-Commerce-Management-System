cdc174a113c82d1d358338fbae67cb8ecd00dcea
lianghuasheng <1053621764@qq.com>
2020-07-15 21:44:00 +0800

    修改了合并上的一些错误

--- src/main/java/com/qf/ecms/exception/ServiceException.java
+++ src/main/java/com/qf/ecms/exception/ServiceException.java
@@ -16,4 +16,20 @@ public class ServiceException extends Exception {
         this.msg = status.getMsg();
         this.status = status.getStatus();
     }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
