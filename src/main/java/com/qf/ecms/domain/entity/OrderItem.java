2249f6eacf5cb44c4bd632ce862fa92f6ac45802
lianghuasheng <1053621764@qq.com>
2020-07-17 10:30:56 +0800

    修改了合并上的一些错误

--- src/main/java/com/qf/ecms/domain/entity/OrderItem.java
+++ src/main/java/com/qf/ecms/domain/entity/OrderItem.java
@@ -6,57 +6,57 @@ import lombok.Data;
 @Data
 public class OrderItem {
     /**
-    * 主键
-    */
+     * 主键
+     */
     private Integer orderItemId;
 
     /**
-    * 商品id
-    */
-    private Integer cid;
-
-    /**
-    * 订单号
-    */
+     * 订单号
+     */
     private Integer orderId;
 
     /**
-    * 会员id
-    */
+     * 会员id
+     */
     private Integer userId;
 
     /**
-    * 商品图片
-    */
+     * 商品图片
+     */
     private String commodityPic;
 
     /**
-    * 商品名称
-    */
+     * 商品名称
+     */
     private String commodityTitle;
 
     /**
-    * 商品价格
-    */
-    private Long commodityPrice;
+     * 商品价格
+     */
+    private BigDecimal commodityPrice;
 
     /**
-    * 购买数量
-    */
+     * 购买数量
+     */
     private Integer commodityQuantity;
 
     /**
-    * 状态,0是启用,1是删除
-    */
+     * 状态,0是启用,1是删除
+     */
     private Integer status;
 
     /**
-    * 颜色
-    */
+     * 颜色
+     */
     private String colorName;
 
     /**
-    * 尺码
-    */
+     * 尺码
+     */
     private String sizeName;
+
+    /**
+     * 商品id
+     */
+    private Integer cid;
 }
